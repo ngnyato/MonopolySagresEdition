ddd201ff71aaa89f273e3f834b44096d58c5ff8a
amgs <user@example.com>
2025-11-19 18:14:00 +0000

    Fix RJ logic.

--- GameController.cs
+++ GameController.cs
@@ -1,15 +1,17 @@
 public class GameController {
     private Dictionary<string,Player> players = new Dictionary<string,Player>();
     public bool HasPlayer(string playerName) {
+        /* Para listas e arrays: */
         // foreach(Player player in players) {
         //     if(player.Name == playerName) {
         //         return true;
         //     }
         // }
+        // return false;
         return players.Keys.Contains(playerName);
     }
 
     public void RegisterPlayer(string playerName){
-        players.Append(new Player(playerName));
+        players.Add(playerName, new Player(playerName));
     }
 }
